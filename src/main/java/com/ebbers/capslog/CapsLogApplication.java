a481cce8660f54c6faea2d3ab2af156537019cdf
Ricardo Ebbers <recl@cin.ufpe.br>
2020-05-03 19:34:32 -0300

    Initialized webflux project

---
+++ src/main/java/com/ebbers/capslog/CapsLogApplication.java
@@ -0,0 +1,13 @@
+package com.ebbers.capslog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapsLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapsLogApplication.class, args);
+    }
+
+}
