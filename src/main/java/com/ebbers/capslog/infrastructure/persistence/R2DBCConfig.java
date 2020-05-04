9d7b573e6da11bd8c9227ce5ab1149d5775af0e8
Ricardo Ebbers <recl@cin.ufpe.br>
2020-05-03 21:08:48 -0300

    Removes redundant bean in R2DBCConfig

--- src/main/java/com/ebbers/capslog/infrastructure/persistence/R2DBCConfig.java
+++ src/main/java/com/ebbers/capslog/infrastructure/persistence/R2DBCConfig.java
@@ -1,12 +1,9 @@
 package com.ebbers.capslog.infrastructure.persistence;
 
-import io.r2dbc.spi.ConnectionFactories;
 import io.r2dbc.spi.ConnectionFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 import org.springframework.data.r2dbc.connectionfactory.init.CompositeDatabasePopulator;
 import org.springframework.data.r2dbc.connectionfactory.init.ConnectionFactoryInitializer;
 import org.springframework.data.r2dbc.connectionfactory.init.ResourceDatabasePopulator;
@@ -14,16 +11,7 @@ import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @Configuration
 @EnableR2dbcRepositories
-public class R2DBCConfig extends AbstractR2dbcConfiguration {
-
-    @Value("${spring.r2dbc.url}")
-    private String url;
-
-    @Override
-    @Bean
-    public ConnectionFactory connectionFactory() {
-        return ConnectionFactories.get(url);
-    }
+public class R2DBCConfig {
 
     @Bean
     public ConnectionFactoryInitializer initializer(ConnectionFactory cf) {
